c2e818dce20f2017e6bef5f14d0a2d25cfadfcca
liuwenchao <s_liuwc@jiedaibao.com>
2016-07-01 17:50:16 +0800

    another test

--- app/src/main/java/com/xiaoliu/demo/mvpdemo/Test.java
+++ app/src/main/java/com/xiaoliu/demo/mvpdemo/Test.java
@@ -2,9 +2,9 @@ package com.xiaoliu.demo.mvpdemo;
 
 /**
  * @author Administrator
- * @date 2016/7/1-17:41
+ * @date 2016/7/1-17:48
  * @desc ${描述类实现的功能}
  */
 
-public interface BaseModel {
+public class Test {
 }
